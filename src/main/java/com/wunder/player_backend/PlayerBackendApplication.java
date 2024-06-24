65299bac5a20d7452846d3bf0235abd90623cbb0
David Lee <133568346+yesdavidleecs@users.noreply.github.com>
2024-06-23 20:46:06 -0700

    first commit

---
+++ src/main/java/com/wunder/player_backend/PlayerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wunder.player_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wunder/player_backend/PlayerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wunder.player_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
