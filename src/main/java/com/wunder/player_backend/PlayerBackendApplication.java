164ce3a581c65e0009cffcc5c3fa03fe23267874
David Lee <133568346+yesdavidleecs@users.noreply.github.com>
2024-06-23 21:24:02 -0700

    new spring boot application is able to build / run with H2 Database as development database and PostgresSQL as prod database

--- src/main/java/com/wunder/player_backend/PlayerBackendApplication.java
+++ src/main/java/com/wunder/player_backend/PlayerBackendApplication.java
@@ -2,6 +2,8 @@ package com.wunder.player_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PlayerBackendApplication {
@@ -10,4 +12,11 @@ public class PlayerBackendApplication {
 		SpringApplication.run(PlayerBackendApplication.class, args);
 	}
 
+	@RestController
+	class HelloController {
+		@GetMapping
+		public String hello() {
+			return "Hello World";
+		}
+	}
 }
